368d68beb6cfcba5be2dfebb3b8de8a51901447c
Leona Currie <leonacurrie1998@gmail.com>
2020-05-30 19:52:00 +0100

    JavaScript sounds

---
+++ script.js
@@ -0,0 +1,83 @@
+var btn1 = document.querySelector(".playButton-1");
+btn1.onclick = function() {
+  const sexDrive = document.querySelector(".sexDrive");
+  sexDrive.play();
+}
+
+var btn2 = document.querySelector(".playButton-2");
+btn2.onclick = function() {
+  const elDiablo = document.querySelector(".elDiablo");
+  elDiablo.play();
+}
+
+var btn3 = document.querySelector(".playButton-3");
+btn3.onclick = function() {
+  const hollywoodWhore = document.querySelector(".hollywoodWhore");
+  hollywoodWhore.play();
+}
+
+var btn4 = document.querySelector(".playButton-4");
+btn4.onclick = function() {
+  const glassHouse = document.querySelector(".glassHouse");
+  glassHouse.play();
+}
+
+var btn5 = document.querySelector(".playButton-5");
+btn5.onclick = function() {
+  const burningMemories = document.querySelector(".burningMemories");
+  burningMemories.play();
+}
+
+var btn6 = document.querySelector(".playButton-6");
+btn6.onclick = function() {
+  const aMessageFromTheCount = document.querySelector(".aMessageFromTheCount");
+  aMessageFromTheCount.play();
+}
+
+var btn7 = document.querySelector(".playButton-7");
+btn7.onclick = function() {
+  const floor13 = document.querySelector(".floor13");
+  floor13.play();
+}
+
+var btn8 = document.querySelector(".playButton-8");
+btn8.onclick = function() {
+  const Roulette = document.querySelector(".Roulette");
+  Roulette.play();
+}
+
+var btn9 = document.querySelector(".playButton-9");
+btn9.onclick = function() {
+  const truckNorris = document.querySelector(".truckNorris");
+  truckNorris.play();
+}
+
+var btn10 = document.querySelector(".playButton-10");
+btn10.onclick = function() {
+  const deathInMyPocket = document.querySelector(".deathInMyPocket");
+  deathInMyPocket.play();
+}
+
+var btn11 = document.querySelector(".playButton-11");
+btn11.onclick = function() {
+  const Candy = document.querySelector(".Candy");
+  Candy.play();
+}
+
+var btn12 = document.querySelector(".playButton-12");
+btn12.onclick = function() {
+  const wasteLove = document.querySelector(".wasteLove");
+  wasteLove.play();
+}
+
+var btn13 = document.querySelector(".playButton-13");
+btn13.onclick = function() {
+  const _53666 = document.querySelector(".53666");
+  _53666.play();
+}
+
+var btn14 = document.querySelector(".playButton-14");
+btn14.onclick = function() {
+  const iThinkImOKAY = document.querySelector(".iThinkImOKAY");
+  iThinkImOKAY.play();
+}
